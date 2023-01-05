183bf9240b89f8a728486e73910bf27894afbcad
ABCXZY <rnrmfjc@gmail.com>
2023-01-05 20:43:55 +0900

    백준 2606 bfs

--- 백준/2606_바이러스(bfs).py
+++ 백준/2606_바이러스(bfs).py
@@ -5,19 +5,22 @@ com = int(input())
 n = int(input())
 edges = [[] for _ in range(com+1)]
 visited = [False for _ in range(com+1)]
+q = []
 
 for _ in range(n):
     a, b = map(int, input().rstrip().split())
     edges[a].append(b)
     edges[b].append(a)
 
-def dfs(node):
-    if visited[node]:
-        return
-    visited[node] = True
-    for i in edges[node]:
-        dfs(i)
+def bfs(node):
+    q.append(node)
+    
+    while q:
+        current = q.pop(0)
+        for i in edges[current]:
+            if visited[i] == False:
+                visited[i] = True
+                q.append(i)
 
-dfs(1)
-
+bfs(1)
 print(sum(visited) - 1)
