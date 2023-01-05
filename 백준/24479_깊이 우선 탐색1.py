88f220a635058f4b45b7ac050f30ee237a10c468
ABCXZY <rnrmfjc@gmail.com>
2023-01-05 18:58:08 +0900

    24479_깊이 우선 탐색1

--- 백준/24479_깊이 우선 탐색1.py
+++ 백준/24479_깊이 우선 탐색1.py
@@ -1,1 +1,39 @@
-print("hello world")
+import sys
+sys.setrecursionlimit(1000000) 
+input = sys.stdin.readline
+
+n, m, r = map(int, input().rstrip().split())
+edges = [[] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
+cnt = 1
+for i in range(m):
+    a, b = map(int, input().rstrip().split())
+    edges[a].append(b)
+    edges[b].append(a)
+    
+for i in range(len(edges)):
+    edges[i].sort()
+
+def dfs(node):
+    global cnt
+    
+    if visited[node] != 0:
+        return
+
+    visited[node] = cnt
+    cnt += 1
+    
+    for i in edges[node]:
+        dfs(i)
+    
+dfs(r)
+for ans in visited[1:]:
+    print(ans)
+"""
+5 5 1
+1 4
+1 2
+2 3
+2 4
+3 4
+"""
