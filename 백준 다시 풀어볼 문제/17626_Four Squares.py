725b3161a4c182d32648057af779797872c6fa59
ABCXZY <rnrmfjc@gmail.com>
2023-02-03 21:07:34 +0900

    11727 2xn 타일링2

--- 백준 다시 풀어볼 문제/17626_Four Squares.py
+++ 백준 다시 풀어볼 문제/17626_Four Squares.py
@@ -6,7 +6,7 @@ dp = [0 for _ in range(50001)]
 
 dp[1] = 1
 for i in range(1, n+1):
-    dp[i] = dp[1] + dp[i - 1]
+    dp[i] = 1 + dp[i - 1]
     for j in range(2, int(i ** (1/2)+1)):
         dp[i] = min(dp[i], 1+dp[i-j*j])
 
