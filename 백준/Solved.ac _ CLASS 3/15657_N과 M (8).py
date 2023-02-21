7aabfcce41bbf191dd7fa654a78618a372209f18
ABCXZY <rnrmfjc@gmail.com>
2023-02-21 21:25:24 +0900

    15657 N과 M (8)

---
+++ 백준/Solved.ac _ CLASS 3/15657_N과 M (8).py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().rstrip().split())
+arr = list(map(int, input().rstrip().split()))
+
+arr.sort()
+def bt(i, depth, l):
+    if depth == M:
+        print(*l)
+        return
+    
+    for j in range(i, len(arr)):
+        bt(j, depth+1, l+[arr[j]])
+
+for i in range(N):
+    bt(i, 1, [arr[i]])
