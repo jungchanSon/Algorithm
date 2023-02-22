bf55c652aeec3d6a7ad25a42a7c03b6ce35e2cb5
ABCXZY <rnrmfjc@gmail.com>
2023-02-22 18:05:47 +0900

    15663

--- 백준/Solved.ac _ CLASS 3/15663_N과 M (9).py
+++ 백준/Solved.ac _ CLASS 3/15663_N과 M (9).py
@@ -5,20 +5,29 @@ arr = list(map(int, input().rstrip().split()))
 visited = [False for _ in range(N)]
 arr.sort()
 
-
-def bt(i, depth, l):
+def bt(depth, l):
+    global temp
+    
     if depth == M:
         print(*l)
         return
+        
+    temp = -1
     
-    for j in range(len(arr)):
-        if visited[j] == False:
+    for j in range(N):
+        if visited[j] == False and arr[j] != temp:
             visited[j] = True
-            bt(j, depth+1, l+[arr[j]])
+            bt(depth+1, l+[arr[j]])
+            temp = arr[j] 
             visited[j] = False
 
-for i in range(N):
-    visited = [False for _ in range(N)]
-    visited[i] = True
-    bt(i, 1, [arr[i]])
+arr_set = set(arr)
+arr_set = list(arr_set)
+arr_set.sort()
+
+# for i in range(len(arr_set)):
+#     visited = [False for _ in range(N)]
+# visited[i] = True
+    
+bt(0, [])
     
