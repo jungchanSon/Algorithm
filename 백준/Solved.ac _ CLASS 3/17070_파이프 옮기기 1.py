c4864cffbc5e6d339243a950eca654f6c6bd1f5e
ABCXZY <rnrmfjc@gmail.com>
2023-02-22 21:25:06 +0900

    17070 파이프 옮기기 bfs

--- 백준/Solved.ac _ CLASS 3/17070_파이프 옮기기 1.py
+++ 백준/Solved.ac _ CLASS 3/17070_파이프 옮기기 1.py
@@ -33,3 +33,34 @@ dp[n][n] = dp[n-1][n-1] + dp[n-1][n] + dp[n][n-1]
 """
 
 print(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2])
+
+#dfs
+# 가로/세로/대각 == shape 0/shape 1/shape 2 
+ans2 = 0
+def dfs(position, shape):
+    global ans2
+    cy = position[0]
+    cx = position[1]
+    if cx == N-1 and cy == N-1:
+        ans2 +=1
+        return
+    
+    if shape == 0:
+        if cx+1 < N and board[cy][cx+1] != 1:
+            dfs((position[0], position[1]+1), 0)
+        if cx+1 < N and cy+1 < N and board[cy+1][cx+1] != 1 and board[cy+1][cx] != 1 and board[cy][cx+1] != 1:
+            dfs((position[0]+1, position[1]+1), 2)
+    elif shape == 1:
+        if cy+1 < N and board[cy+1][cx] != 1:
+            dfs((cy+1, cx), 1)
+        if cy+1 < N and cx+1 < N and board[cy+1][cx+1] != 1 and board[cy+1][cx] != 1 and board[cy][cx+1] != 1:
+            dfs((cy+1, cx+1), 2)
+    elif shape == 2:
+        if cx+1< N and board[cy][cx+1] != 1:
+            dfs((cy, cx+1), 0)
+        if cy+1 < N and board[cy+1][cx] != 1:
+            dfs((cy+1, cx), 1)
+        if cy+1 < N and cx+1 <N and board[cy+1][cx+1] != 1 and board[cy+1][cx] != 1 and board[cy][cx+1] != 1:
+            dfs((cy+1, cx+1), 2)
+dfs((0, 1), 0)
+print(ans2)
