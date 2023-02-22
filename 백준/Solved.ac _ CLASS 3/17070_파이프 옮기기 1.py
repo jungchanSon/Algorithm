2b75143fe5ad77978e67907c062fe2183a714ca3
ABCXZY <rnrmfjc@gmail.com>
2023-02-22 21:08:53 +0900

    17070 파이프 옮기기 -> dp

--- 백준/Solved.ac _ CLASS 3/17070_파이프 옮기기 1.py
+++ 백준/Solved.ac _ CLASS 3/17070_파이프 옮기기 1.py
@@ -19,8 +19,10 @@ for i in range(N):
 
 for i in range(1, N):
     for j in range(2, N):
-        dp[i][j][0] = dp[i][j-1][2] + dp[i][j-1][0]
-        dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
+        if board[i][j] != 1 and board[i][j-1] != 1:
+            dp[i][j][0] = dp[i][j-1][2] + dp[i][j-1][0]
+        if board[i][j] != 1 and board[i-1][j] != 1:
+            dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
         if board[i][j] != 1 and board[i-1][j-1] != 1 and board[i-1][j] != 1 and  board[i][j-1] != 1:
             dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2]
 # for i in dp:
