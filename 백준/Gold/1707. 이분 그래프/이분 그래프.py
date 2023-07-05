5679ba97db25bb4443cb3c34e16b19bede3198b5
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2023-07-05 20:35:17 +0900

    Update 이분 그래프.py

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,76 +1,44 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+test_num = int(input())
 
-# 테스트 케이스의 개수
-t = int(sys.stdin.readline())
+fail = False
 
-# 현재 노드와 이웃한 노드 탐색
-def dfs(node):
-
-    global result
-
-    for neighbor in graph[node]:
-
-        # 이웃 노드에 색칠이 되어있지 않다면
-        if (visited[neighbor] == -1):
-
-            # 현재 노드가 1로 색칠되어있다면
-            if (visited[node] == 1):
-                visited[neighbor] = 2
-            # 현재 노드가 2로 색칠되어있다면
-            if (visited[node] == 2):
-                visited[neighbor] = 1
-
-            dfs(neighbor)
-
-        # 이웃 노드에 색칠이 되어있다면
-        else:
-
-            # 현재 노드와 동일한 색깔이라면
-            if (visited[node] == visited[neighbor]):
-
-                result = False
-                return
-
-for _ in range(t):
-
-    # 정점의 개수 V와 간선의 개수 E
-    v, e = map(int, sys.stdin.readline().split())
-
-    # 색칠 여부 저장
-    visited = [-1] * (v+1)
-
-    # 양방향 간선 정보 저장
+def dfs(cur, cur_color):
+    global fail
+    
+    if v_color[cur] == cur_color * -1:
+        fail = True
+        return
+    if not v_color[cur] : 
+        v_color[cur] = cur_color
+            
+        for i in graph[cur]:
+            dfs(i, cur_color*-1)
+        
+for _ in range(test_num):
+    v, e = map(int, input().rsplit())
     graph = [[] for _ in range(v+1)]
+    v_color = [0 for _ in range(v+1)]  
 
-    # 양방향 간선 정보 저장
+    fail = False
+    visited = [False for _ in range(v+1)]
+            
     for _ in range(e):
-        start, end = map(int, sys.stdin.readline().split())
-        graph[start].append(end)
-        graph[end].append(start)
-
-    # 이분 그래프 여부
-    result = True
-
-    # 모든 노드 탐색
+        s, e = map(int, input().rsplit())
+        graph[s].append(e)
+        graph[e].append(s)
+        
+    cur_color = 1
+            
     for i in range(1, v+1):
-
-        # 현재 노드가 색칠 되어있지 않다면
-        if (visited[i] == -1):
-
-            # 현재 노드를 색칠
-            visited[i] = 1
-
-            # 이웃한 노드에 대해서 색칠
-            dfs(i)
-
-            # 현재 노드로부터 이분 그래프가 아닌것을 확인했다면
-            if (result == False):
+        if not v_color[i] and graph[i]:
+            dfs(i, cur_color)
+            if fail == True:
                 break
-
-    # 이분 그래프가 아니라면
-    if (result == False):
+            
+    if fail:
         print("NO")
-    # 이분 그래프라면
     else:
         print("YES")
