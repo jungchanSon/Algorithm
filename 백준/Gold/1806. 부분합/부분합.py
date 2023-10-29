953509e56f85a187be08a703b868ce120f5bd180
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2023-10-29 21:06:13 +0900

    [Gold IV] Title: 부분합, Time: 136 ms, Memory: 121844 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,47 +1,31 @@
 import sys
-import math
+import bisect
 input = sys.stdin.readline
 
-N, S = map(int, input().rstrip().split())
-arr = list(map(int, input().rstrip().split()))
-ans = math.inf
+n, s = map(int, input().rsplit())
 
-# dp 누접합 완전탐색 -> 시간 초과.
+arr = list(map(int, input().rsplit()))
 
-# dp = [0 for _ in range(N)]
-# dp[0] = arr[0]
+ans =  1e10
+sums = 0
 
-# for i in range(1,N):
-#     dp[i] = dp[i-1] + arr[i]
-
-# for i in range(N):
-#     if dp[i] >= S:
-#         ans = i+1
-#         break
+st = 0
+en = 0
+f = False
+while st <= en and st < n and en <= n:
     
-# if ans == math.inf:
-#     print(0)
-
-# for i in range(1, N):
-#     for j in range(i, N):
-#         if dp[j] - dp[i-1] >= S:
-#             ans = min(ans, j-i+1)
-
-arr.append(0)
-start = 0
-end = 0
-current_sum = arr[0]
-
-while start <= end and end < N:
-    if current_sum < S:
-        end += 1
-        current_sum += arr[end]
-    else:
-        ans = min(ans, end-start+1)
+    if sums < s:
+        if en == n:
+            break
+        sums += arr[en]
+        en += 1
+    elif sums >= s:
+        ans = min(ans, en-st)
+        sums -= arr[st]
+        st += 1
+        f = True
         
-        current_sum -= arr[start]
-        start += 1
-if ans == math.inf:
-    print(0)
-else:
+if f:
     print(ans)
+else:
+    print(0)
