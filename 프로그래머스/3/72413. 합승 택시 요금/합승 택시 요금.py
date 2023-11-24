d6eb1f40194d414847c3fb7b6a5b474722f7bf71
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2023-11-24 20:37:47 +0900

    [level 3] Title: 합승 택시 요금, Time: 44.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,33 +1,31 @@
-import copy
 import heapq
 
 def solution(n, s, a, b, fares):
-    board = [[] for _ in range(n+1)]
-    visited = [False for _ in range(n+1)]
-
-    for start, end, w in fares:
-        board[start].append((end, w))
-        board[end].append((start, w))
-        
-    def dijstra(start, end):
-        dist = [1e9 for _ in range(n+1)]
-        
+    answer = 0
+    graph = [[] for _ in range(n+1)]
+    d = [1e6 for _ in range(n+1)]
+    for start, e, c in fares:
+        graph[start].append([e, c])
+        graph[e].append([start, c])
+    
+    def dij(s, e):
         q = []
-        heapq.heappush(q, (0, start))
-        dist[start] = 0
+        heapq.heappush(q, (0, s)) 
+        d = [1e6 for _ in range(n+1)]
+        d[s] = 0
         while q:
-            d, nn = heapq.heappop(q)
-            if dist[nn] < d:
+            dist, now = heapq.heappop(q)
+            if d[now] < dist:
                 continue
-            for next in board[nn]:
-                cost = dist[nn] + next[1]
-                if cost < dist[next[0]]:
-                    dist[next[0]] = cost
-                    heapq.heappush(q, (cost, next[0]))
-        return dist[end]
+            for nxt in graph[now]:
+                cost = d[now] + nxt[1]
+                if cost < d[nxt[0]]:
+                    d[nxt[0]] = cost
+                    heapq.heappush(q, (cost, nxt[0])) 
+        return d[e]
     
-    ans = 1e9
+    answer = dij(s, a) + dij(s, b)
+
     for i in range(1, n+1):
-        ans = min(ans, dijstra(s, i) + dijstra(i, a) + dijstra(i, b))
-    
-    return ans
+        answer = min(answer, dij(s, i) + dij(i, a) + dij(i, b))
+    return answer
