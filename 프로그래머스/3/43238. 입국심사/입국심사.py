34e1b07f70a8dc59129539d89a1bd2b02836409e
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2024-03-19 21:16:29 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,21 +1,6 @@
 def solution(n, times):
     answer = 0
-    l, r = 1, 1_000_000_000 * 1_000_000_000
-    
-    while l <= r: 
-        m = (l+r) // 2 
-        temp = 0
-        
-        for i in times:
-            temp += m // i
-          
-        
-        if temp >= n:
-            answer = m
-            r = m - 1
-        else:
-            l = m + 1
-        
-    
     return answer
 
+1 1 1 1  1  1 
+7 7 7 10 10 10 
