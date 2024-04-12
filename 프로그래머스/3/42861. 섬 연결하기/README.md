6a622e51493d50694b541d26061ac1ccd86960dd
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2024-04-13 08:51:24 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 10.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 13일 13:31:32
+2024년 04월 13일 08:51:21
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,26 +1,29 @@
 def solution(n, costs):
     answer = 0
-    costs.sort(key=lambda x: x[2])
-    visited = [False for _ in range(n)]
-    graph = [[0 for _ in range(n)] for _ in range(n)]
-    parent = [i for i in range( n+1)]
+    p = [i for i in range(n+1)]
     
-    def find(a):
-        if parent[a] != a:
-            parent[a] = find(parent[a])
-        return parent[a]
-    def union(a, b):
-        a = find(a)
-        b = find(b)
-        if a < b :
-            parent[b] = a
+    def find(x):
+        if x == p[x]:
+            return x
+        p[x] = find(p[x])
+        return p[x]
+    
+    def union(x, y):
+        x, y = find(x), find(y)
+        
+        if x < y :
+            p[y] = x
         else:
-            parent[a] = b
+            p[x] = y
             
-    for i, j, k in costs:
-        if find(i) != find(j):
-            union(i, j)
-        else: 
-            continue
-        answer += k
+    costs.sort(key=lambda x: x[2])
+    print(costs)
+    
+    while costs:
+        a, b, c = costs.pop(0)
+        a += 1
+        b += 1
+        if find(a) != find(b):
+            union(a, b)
+            answer += c
     return answer
