75f7b73ae7488af525f8ae2b1e529ff1b22e2419
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2024-04-16 20:14:53 +0900

    [Gold III] Title: 줄 세우기, Time: 212 ms, Memory: 121136 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,33 +1,30 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-N, M = map(int, input().rstrip().split())
-in_degree = [0 for _ in range(N+1)]
-arr = [[] for _ in range(N+1)]
+n, m = map(int, input().rsplit())
+graph = [[] for _ in range(n+1)]
+indegree = [0 for _ in range(n+1)]
 q = deque()
 
-for _ in range(M):
-    a, b = map(int, input().rstrip().split())
-    arr[a].append(b)
-    in_degree[b] += 1
-    
-for i in range(1, N+1):
-    if in_degree[i] == 0:
-        q.append(i)
-# print(q)
-# for i in arr:
-#     print(i)
+for _ in range(m):
+    a, b = map(int, input().rsplit())
+    graph[a].append(b)
+    indegree[b] += 1
+
 ans = []
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        q.append(i)
+        ans.append(i)
 while q:
     cur = q.popleft()
-    ans.append(cur)
-    next_node = arr[cur]
-    for i in next_node:
-        in_degree[i] -= 1
-        if in_degree[i] == 0:
+    
+    for i in graph[cur]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
             q.append(i)
+            ans.append(i)
 
 print(*ans)
-        
-#위상 정렬
