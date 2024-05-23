41b46910caac02bca360f4db4730c15c3da52e38
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2024-05-23 21:23:03 +0900

    [Silver II] Title: 촌수계산, Time: 140 ms, Memory: 109628 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,31 @@
+import heapq
+import sys
+import copy
+input = sys.stdin.readline
+inf = 1e9
+
+n = int(input())
+a, b = map(int, input().rsplit())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    c, p = map(int, input().rsplit())
+    graph[c].append(p)
+    graph[p].append(c)
+
+visited = [False for _ in range(n+1)]
+q = [(a, 0)]
+check = True
+while q: 
+    c, l = q.pop()
+    if c == b:
+        print(l)
+        check =False
+    for k in graph[c]:
+        if visited[k]:
+            continue
+        q.append((k, l+1))
+        visited[k] = True
+
+if check:
+    print(-1)
