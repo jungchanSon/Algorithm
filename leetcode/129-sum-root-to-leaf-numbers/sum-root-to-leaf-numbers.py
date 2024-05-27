72ed029ce8abce88ea181b023ca729820cac8d15
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2024-05-27 19:04:58 +0900

    Time: 32 ms (77.20%) | Memory: 16.4 MB (77.27%) - LeetSync

--- leetcode/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ leetcode/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -14,10 +14,8 @@ class Solution:
                 ans += nv
 
             if node.left:
-                print("left")
                 recursive(node.left, nv)
             if node.right:
-                print('right')
                 recursive(node.right, nv)
 
         recursive(root, 0)
