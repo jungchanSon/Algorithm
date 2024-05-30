be4bc79a854c84dce5fe35f80bde1ce38b598143
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2024-05-30 15:25:05 +0900

    Time: 42 ms (79.18%) | Memory: 16.5 MB (66.13%) - LeetSync

--- leetcode/13-roman-to-integer/roman-to-integer.py
+++ leetcode/13-roman-to-integer/roman-to-integer.py
@@ -8,16 +8,16 @@ class Solution:
         for i, v in enumerate(symbol):
             symbol_value[v] = value[i]
         
+       
         prev = ""
-        special = ("I", "X", "C")
-        special_dict = dict()
-        special_dict["I"] = ("V", "X")
-        special_dict["X"] = ("L", "C")
-        special_dict["C"] = ("D", "M")
-
         for i in s:
-            if prev in special and i in special_dict[prev] :
+            if prev == "C" and (i == "D" or i == "M"):
                 answer -= 2*symbol_value[prev]
+            if prev == "X" and (i == "L" or i == "C"):
+                answer -= 2*symbol_value[prev]
+            if prev == "I" and (i == "V" or i == "X"):
+                answer -= 2*symbol_value[prev]
+
             answer += symbol_value[i]
             prev = i
         
