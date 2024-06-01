1f599a635e4c145fda3a4b1b6e162cf205b0104d
jungchanSon <77601495+jungchanSon@users.noreply.github.com>
2024-06-01 18:03:49 +0900

    Time: 41 ms (27.33%) | Memory: 16.5 MB (89.23%) - LeetSync

---
+++ leetcode/27-remove-element/remove-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while True:
+            try:
+                nums.remove(val)
+            except:
+                return len(nums)
